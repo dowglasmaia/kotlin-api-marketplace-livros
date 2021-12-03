b6590bd43b26bcdc1250a6093051316ab0452663
Dowglas Maia <dowglasmaia@gmail.com>
2021-12-02 23:13:33 -0300

    Criando getBy Id e update

--- src/main/kotlin/com/dwmaia/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/dwmaia/mercadolivro/controller/CustomerController.kt
@@ -1,10 +1,12 @@
 package com.dwmaia.mercadolivro.controller
 
 import com.dwmaia.mercadolivro.controller.request.PostCustomerDTO
+import com.dwmaia.mercadolivro.controller.request.PutCustomerDTO
 import com.dwmaia.mercadolivro.model.CustomerModel
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import kotlin.collections.filter
 
 @RestController
 @RequestMapping("custumers")
@@ -13,10 +15,30 @@ class CustomerController {
     val customers = mutableListOf<CustomerModel>()
 
     @GetMapping()
-    fun getCustomer(): ResponseEntity<List<CustomerModel>> {
+    fun getAll(): ResponseEntity<List<CustomerModel>> {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(customers)
+    }
+
+    @GetMapping("/{id}")
+    fun getCustomer(@PathVariable id: String): ResponseEntity<CustomerModel> {
+        val customer = customers.filter { it.id == id }.first()
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(customer)
+    }
+
+    @PutMapping("/{id}")
+    fun update(@PathVariable id: String, @RequestBody request: PutCustomerDTO): ResponseEntity<Void> {
+        customers.filter { it.id == id }.first().let {
+            it.email = request.email
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build()
     }
 
     @PostMapping()
---
+++ src/main/kotlin/com/dwmaia/mercadolivro/controller/request/PutCustomerDTO.kt
@@ -0,0 +1,5 @@
+package com.dwmaia.mercadolivro.controller.request
+
+data class PutCustomerDTO (
+     var email: String
+)
