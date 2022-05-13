5500b8f8dc93b520db8bbd3848d3d94fd9af44ac
Dowglas Maia <dowglasmaia@gmail.com>
2022-05-12 23:15:29 -0300

    impl Services e mapper

--- src/main/kotlin/com/dwmaia/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/dwmaia/mercadolivro/controller/CustomerController.kt
@@ -1,43 +1,55 @@
 package com.dwmaia.mercadolivro.controller
 
 import com.dwmaia.mercadolivro.controller.request.PostCustomerDTO
+import com.dwmaia.mercadolivro.controller.request.PutCustomerDTO
+import com.dwmaia.mercadolivro.extension.toCustomerModel
 import com.dwmaia.mercadolivro.model.CustomerModel
+import com.dwmaia.mercadolivro.service.CustomerService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("custumers")
-class CustomerController {
-
-    val customers = mutableListOf<CustomerModel>()
-
-    @GetMapping()
-    fun getCustomer(): ResponseEntity<List<CustomerModel>> {
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(customers)
-    }
+class CustomerController(
+        val customerService: CustomerService
+) {
 
     @PostMapping()
     fun create(@RequestBody request: PostCustomerDTO): ResponseEntity<Void> {
-        println(request)
-
-        var id = generationId()
-
-        customers.add(CustomerModel(id, request.name, request.email))
-
+        customerService.create(request.toCustomerModel())
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @GetMapping()
+    fun getAll(@RequestParam name: String?): ResponseEntity<List<CustomerModel>> {
+        val resopnse = customerService.getAll(name)
+        return ResponseEntity.status(HttpStatus.OK).body(resopnse);
+    }
 
-    private fun generationId(): String {
-        var id = if (customers.isEmpty()) {
-            1
-        } else {
-            customers.last().id.toInt() + 1
-        }
-        return id.toString()
+    @GetMapping("/{id}")
+    fun getCustomer(@PathVariable id: String): ResponseEntity<CustomerModel> {
+        val response = customerService.getCustomer(id)
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response)
+    }
+
+    @PutMapping("/{id}")
+    fun update(@PathVariable id: String, @RequestBody request: PutCustomerDTO): ResponseEntity<Void> {
+        customerService.update(id, request.toCustomerModel());
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build()
+    }
+
+
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable id: String): ResponseEntity<Void> {
+        customerService.delete(id)
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build()
     }
 
 
